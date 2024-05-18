92bef134eeeff2ba0bf16d163bb4d5b73a16b4d4
rohitkadam366 <117926887+rohitkadam366@users.noreply.github.com>
2024-05-19 02:49:01 +0530

    Add_model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import Product
 # Register your models here.
+
+@admin.register(Product)
+class productModelAdmin(admin.ModelAdmin):
+    list_display= ['id','title','selling_price','discounted_price','description','composition','prodapp','category']
