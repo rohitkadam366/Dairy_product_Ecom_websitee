bfee60ddebc45a01c6c9c73bf9c83380fa21907a
rohitkadam366 <117926887+rohitkadam366@users.noreply.github.com>
2024-05-20 02:23:02 +0530

    product_category_code_complete

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index,name='index')
-]
+    path('',views.index,name='index'),
+    path('category/<slug:val>',views.CategoryView.as_view(),name='category'),
+    path('product_details/<int:pk>',views.ProductDetail.as_view(),name='product_details')
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from . models import Product
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+
+class CategoryView(View):
+    def get(self, request,val):
+        product=Product.objects.filter(category=val)
+        title=Product.objects.filter(category=val).values('title')
+
+        return render(request, 'app/category.html',locals())
+
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        return render(request,'app/product_details.html',locals())
